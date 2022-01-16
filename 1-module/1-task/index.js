de05d63ddd46de2d88662d56ca23afa0dfcd4db6
ALEX <alexanderR24@yandex.ru>
2022-01-16 16:00:52 +0400

    test

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,15 +1,3 @@
 function factorial(n) {
   // ваш код...
-  let f = 1;
-
-  do {
-    if (n == 0) {
-      f = 1;
-      break;
-    }
-    f *= n;
-    n -= 1;
-  } while (n > 1);
-
-  return f;
 }
