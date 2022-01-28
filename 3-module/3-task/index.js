af37bd763d45de3a900ea9096289e1b6be4a9ac5
Alex <alexanderR24@yandex.ru>
2022-01-28 21:17:20 +0400

    dz 3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
 function camelize(str) {
   // ваш код...
+  return str.split('-')
+         .map((item, i) => i == 0 ? item : item.slice(0, 1).toUpperCase() + item.slice(1))
+         .join('');
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
   // ваш код...
+  let filterUserAge = users.filter(item => item.age <= age);
+
+  let str = '';
+  
+  filterUserAge.forEach((item, i) => {
+    str += (i == filterUserAge.length - 1) ? `${item.name}, ${item.balance}` : `${item.name}, ${item.balance}\n`;
+  });
+
+  return str;
 }
