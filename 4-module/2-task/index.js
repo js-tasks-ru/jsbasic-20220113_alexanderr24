16fbd92d03cca9f1ca495f4aed703df1882816c4
Alex <alexanderR24@yandex.ru>
2022-02-01 21:16:33 +0400

    Homework 4-module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
   // ваш код...
+  let getCountElemets = Array.from(table.rows).length;
+  for (let i = 0; i < getCountElemets; i++) {
+    table.rows[i].cells[i].style.backgroundColor = "red";
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,42 @@
 function highlight(table) {
   // ваш код...
+  let arr = Array.from(table.lastElementChild.rows);
+
+  arr.forEach((item) => {
+    let tdLastElements = item.lastElementChild;
+
+    if (tdLastElements.hasAttribute("data-available")) {
+      switch (tdLastElements.getAttribute("data-available")) {
+        case "true":
+          item.classList.add("available");
+          break;
+        case "false": {
+          item.classList.add("unavailable");
+        }
+      }
+    } else {
+      item.setAttribute("hidden", "");
+    }
+  });
+
+  arr.forEach((item) => {
+    let tdGender = item.children[2];
+    switch (tdGender.textContent) {
+      case "m": {
+        item.classList.add("male");
+        break;
+      }
+      case "f": {
+        item.classList.add("female");
+        break;
+      }
+    }
+  });
+
+  arr.forEach((item) => {
+    let tdAge = item.children[1];
+    if (tdAge.textContent < 18) {
+      item.style.textDecoration = "line-through";
+    }
+  });
 }
