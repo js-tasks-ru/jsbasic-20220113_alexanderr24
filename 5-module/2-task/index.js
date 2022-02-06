0e026c35fabe81f0d57f5c325d1bed1f1c07565d
Alex <alexanderR24@yandex.ru>
2022-02-06 20:30:07 +0400

    Homework 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
   // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = btn.nextElementSibling;
+
+  btn.addEventListener('click', () => {
+    text.hidden = !text.hidden;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
   // ваш код...
+
+  let carousel = document.querySelector('.carousel');
+  let carouselInner = carousel.querySelector('.carousel__inner');
+  let carouselSlides = carousel.querySelectorAll('.carousel__slide');
+  let carouselArrowLeft = carousel.querySelector('.carousel__arrow_left');
+  let carouselArrowRight = carousel.querySelector('.carousel__arrow_right');
+
+  let offset = 0;
+  let getSlideWidth = carousel.offsetWidth;
+  let getSlidesWidth = 0;
+
+  carouselSlides.forEach(item => {
+    item.style.width = `${getSlideWidth}px`;
+    getSlidesWidth += item.offsetWidth;
+  });
+
+  carouselArrowRight.addEventListener('click', () => {
+    if (offset != getSlidesWidth - getSlideWidth) offset += getSlideWidth;
+    if (offset == getSlidesWidth - getSlideWidth) carouselArrowRight.style.display = 'none';
+    if (offset > 0) carouselArrowLeft.style.display = '';;
+    carouselInner.style.transform = `translateX(-${offset}px)`;
+  });
+
+  carouselArrowLeft.style.display = 'none';
+
+  carouselArrowLeft.addEventListener('click', () => {
+    if (offset < getSlidesWidth) carouselArrowRight.style.display = '';
+    if (offset != 0) offset -= getSlideWidth;
+    if (offset == 0) carouselArrowLeft.style.display = 'none';
+    carouselInner.style.transform = `translateX(-${offset}px)`;
+  });
+
 }
