d331f1cb7a114850c2264761fc7a50a3855cbdec
Alex <alexanderR24@yandex.ru>
2022-04-30 20:43:50 +0400

    dz 6 module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,47 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.template();
+    this.deleteUser();
+  }
+
+  template() {
+    let table = document.createElement('table');
+    table.innerHTML = `
+        <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      ${this.rows.map(item => {
+        return `
+          <tr>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.salary}</td>
+            <td>${item.city}</td>
+            <td><button>X</button></td>
+          </tr>
+        `;
+     }).join('')
+      }
+      </tbody>
+    `;
+
+    return table;
+  }
+
+  deleteUser() {
+    this.elem.querySelectorAll('button').forEach(item => {
+      item.addEventListener('click', () => {
+        item.parentElement.parentElement.remove();
+      });
+    });
   }
 }
